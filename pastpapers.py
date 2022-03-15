469117d7a93ef2455ddc983c9d98a3df97dcdeaf
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2022-03-15 16:40:39 +0530

    Added 2019 web programming scheme.

--- pastpapers.py
+++ pastpapers.py
@@ -452,6 +452,7 @@ class PastPapers():
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/fourth-semester/web/2017-web.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/fourth-semester/web/2018-web-scheme.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/fourth-semester/web/2018-web.pdf',
+            'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/fourth-semester/web/2019-web-scheme.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/fourth-semester/web/2019-web.pdf']
 
         bot.send_message(message.chat.id, str(len(files)) + " File/s Incoming...")
