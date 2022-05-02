fdb4ec6597bfbd03186387796c8ff5e9096544be
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2022-05-02 22:18:53 +0530

    Added 3rd semester 2017 and 2018 RAD papers.

--- pastpapers.py
+++ pastpapers.py
@@ -315,7 +315,9 @@ class PastPapers():
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2016-rad-scheme.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2016-rad.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2017-rad-scheme.pdf',
+            'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2017-rad.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2018-rad-scheme.pdf',
+            'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2018-rad.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2019-rad-scheme.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2019-rad.pdf',
             'https://github.com/pasindujr/HnditHelperBot_pdfs/raw/main/papers/third-semester/rad/2020-rad.pdf']
